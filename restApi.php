685a9487bc5477dd16aac957f0901e3bff55f191
msulhanek <m.sulhane@gmail.com>
2020-06-04 11:58:54 +0200

    Upraveny tlmic

---
+++ restApi.php
@@ -0,0 +1,193 @@
+<?php
+session_start();
+// get the HTTP method, path and body of the request
+$method = $_SERVER['REQUEST_METHOD'];
+//var_dump($method); echo("<br>");
+$request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+$input = json_decode(file_get_contents('php://input'), true);   // php://input - read raw data from the request body
+$key = array_shift($request) + 0;
+
+
+switch ($method) {
+    case 'GET':
+        $result = "GET" . $key;
+        break;
+    case 'PUT':
+        $result = "PUT" . $key;
+        break;
+    case 'POST':
+        $result = "POST" . $key;
+        break;
+    case 'DELETE':
+        $result = "DELETE" . $key;
+        break;
+}
+
+
+
+
+if ($method == 'GET') {
+
+    switch ($_GET["action"]) {
+        case "vykonajPrikaz":
+            $command = $_GET['prikaz'];
+
+            $output = ltrim(shell_exec('octave --no-gui --quiet --eval "pkg load control;'. $command .'"'));
+
+            echo $output;
+//            $cmd = "octave -qf --eval 'printf (\"%f\", " . $_GET['r'] . ");'";
+//            $output = exec ($cmd);
+//            var_dump ($output);
+
+
+
+
+            break;
+        case "getDataKyvadlo":
+            $r = $_GET['r'];
+
+
+            $lastP = [0,0,0,0];
+
+            $command = "M = .5;
+                        m = 0.2;
+                        b = 0.1;
+                        I = 0.006;
+                        g = 9.8;
+                        l = 0.3;
+                        p = I*(M+m)+M*m*l^2;
+                        A = [0 1 0 0; 0 -(I+m*l^2)*b/p (m^2*g*l^2)/p 0; 0 0 0 1; 0 -(m*l*b)/p m*g*l*(M+m)/p 0];
+                        B = [ 0; (I+m*l^2)/p; 0; m*l/p];
+                        C = [1 0 0 0; 0 0 1 0];
+                        D = [0; 0];
+                        K = lqr(A,B,C'*C,1);
+                        Ac = [(A-B*K)];
+                        N = -inv(C(1,:)*inv(A-B*K)*B);
+                        
+                        sys = ss(Ac,B*N,C,D);
+                        r = " . $r . ";
+                        t = 0:0.05:10;
+                        [y,t,x]=lsim(sys,r*ones(size(t)),t,[". implode(",", $lastP) ."]);
+                        disp(x(:,1))
+                        disp('endOfPos')
+                        disp(x(:,3)) 
+                        ";
+
+            $output = ltrim(shell_exec('octave --no-gui --quiet --eval "pkg load control;'. $command .'"'));
+            $parts = preg_split('/\s+/', $output);
+
+//           var_dump($parts);
+            $pos=true;
+            $positions=[];
+            $angles=[];
+            $i=0;
+
+            foreach ($parts as $part){
+                if($part==="endOfPos"){
+                    $pos=false;
+                    $i=0;
+                    continue;
+                }
+                if ($pos){
+                    array_push($positions,doubleval($part));
+                    array_push($datapoints1,array("x" => $i, "y"=>doubleval($part)));
+
+                }else{
+                    array_push($angles,doubleval($part));
+                    array_push($datapoints2,array("x" => $i, "y"=>doubleval($part)));
+                }
+                $i++;
+            }
+            array_pop($angles);
+            echo "kyvadlo";
+//            var_dump($parts);
+//            var_dump($positions);
+            $_SESSION['hello']= "kyvadlo";
+            $_SESSION['pos']= $positions;
+
+            $_SESSION['ang']= $angles;
+
+
+
+
+
+            break;
+        case "getDataTlmic":
+            $r = $_GET['r'];
+
+
+            $lastP = [0,0,0,0];
+
+            $command = "
+                        
+                        m1 = 2500; m2 = 320;
+                        k1 = 80000; k2 = 500000;
+                        b1 = 350; b2 = 15020;
+                        A=[0 1 0 0;-(b1*b2)/(m1*m2) 0 ((b1/m1)*((b1/m1)+(b1/m2)+(b2/m2)))-(k1/m1) -(b1/m1);b2/m2 0 -((b1/m1)+(b1/m2)+(b2/m2)) 1;k2/m2 0 -((k1/m1)+(k1/m2)+(k2/m2)) 0];
+                        B=[0 0;1/m1 (b1*b2)/(m1*m2);0 -(b2/m2);(1/m1)+(1/m2) -(k2/m2)];
+                        C=[0 0 1 0]; D=[0 0];
+                        Aa = [[A,[0 0 0 0]'];[C, 0]];
+                        Ba = [B;[0 0]];
+                        Ca = [C,0]; Da = D;
+                        K = [0 2.3e6 5e8 0 8e6];
+                        sys = ss(Aa-Ba(:,1)*K,Ba,Ca,Da);
+                        
+                        t = 0:0.01:5;
+                         r = " . $r . ";
+                        initX1=0;
+                        initX1d=0;
+                        initX2=0;
+                        initX2d=0;
+                        [y,t,x]=lsim(sys*[0;1],r*ones(size(t)),t,[initX1;initX1d;initX2;initX2d;0]);
+                        disp(x(:,1))
+                        disp('endOfPos')
+                        disp(x(:,3)) 
+                        ";
+
+            $output = ltrim(shell_exec('octave --no-gui --quiet --eval "pkg load control;'. $command .'"'));
+            $parts = preg_split('/\s+/', $output);
+
+//           var_dump($parts);
+            $pos=true;
+            $positions=[];
+            $angles=[];
+            $i=0;
+
+            foreach ($parts as $part){
+                if($part==="endOfPos"){
+                    $pos=false;
+                    $i=0;
+                    continue;
+                }
+                if ($pos){
+                    array_push($positions,doubleval($part));
+                    array_push($datapoints1,array("x" => $i, "y"=>doubleval($part)));
+
+                }else{
+                    array_push($angles,doubleval($part));
+                    array_push($datapoints2,array("x" => $i, "y"=>doubleval($part)));
+                }
+                $i++;
+            }
+            array_pop($angles);
+//            var_dump($parts);
+//            var_dump($positions);
+
+
+            $_SESSION['pos']= $positions;
+
+            $_SESSION['ang']= $angles;
+
+
+            break;
+
+    }
+} elseif ($method == 'POST') {
+
+
+//    echo json_encode($result);
+
+} else {
+    echo json_encode($result);
+
+}
--- tlmic.php
+++ tlmic.php
@@ -1,5 +1,21 @@
 <?php
 
+if (isset($_GET['R'])) {
+    session_start();
+    $positions = [];
+    $angles = [];
+
+    $positions = $_SESSION['pos'];
+    $angles = $_SESSION['ang'];
+}
+else{
+    $positions = [];
+    $angles = [];
+    $_SESSION['pos'] = [];
+    $_SESSION['ang'] = [];
+}
+
+
 ?>
 
 <!doctype html>
@@ -11,9 +27,29 @@
     <link rel="stylesheet" href="style.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-material-design@4.1.1/dist/css/bootstrap-material-design.min.css" integrity="sha384-wXznGJNEXNG1NFsbm0ugrLFMQPWswR3lds2VeinahP8N0zJw9VWSopbjv2x7WCvX" crossorigin="anonymous">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <?php
+
+    if (isset($_GET['R'])) {
+        echo "<script>
+        $.ajax({
+                    type: 'GET',
+                    url: 'http://147.175.121.210:8067/skuskoveZadanie/restApi.php/kyvadlo?action=getDataTlmic&r=" . $_GET['R'] . "',
+                    success: function (msg) {
+                        $(\"#output1\").html(msg);
+                    }
+                });           
+</script>";
+
+    }
+
+    ?>
+
     <title>Záverečný projekt</title>
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
 </head>
 <body>
+<nav>
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
             <a class="navbar-brand" href="index.php">Domov</a>
@@ -23,13 +59,13 @@
             <div class="collapse navbar-collapse mr-3" id="navbarNav">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="kyvadlo.php">Kyvadlo <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="kyvadlo.php">Kyvadlo</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="gulicka.php">Gulička</a>
                     </li>
-                    <li class="nav-item active">
-                        <a class="nav-link" href="tlmic.php">Tlmič</a>
+                    <li class="nav-item">
+                        <a class="nav-link active" href="tlmic.php">Tlmič</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" href="lietadlo.php">Lietadlo</a>
@@ -40,25 +76,25 @@
                 </ul>
 
                 <div class="row col-12">
-                    <a class="nav-link offset-6 active btn btn-dark col-xs-2" href="tlmic.php"><i id="slovakiaIcon"></i></a>
-                    <a class="nav-link btn btn-dark col-xs-2" href="tlmicEN.php"><i id="ukIcon"></i></a>
+                    <a class="nav-link offset-6 active btn btn-dark col-xs-2" href="kyvadlo.php"><i id="slovakiaIcon"></i></a>
+                    <a class="nav-link btn btn-dark col-xs-2" href="kyvadloEN.php"><i id="ukIcon"></i></a>
                 </div>
 
             </div>
         </nav>
     </div>
-
+</nav>
 
 <div class="container">
     <div class="jumbotron">
 
-        <h1 class="display-5">Gulička na tyči</h1>
+        <h1 class="display-5">Tlmič</h1>
         <hr>
         <form action="tlmic.php" method="get">
             <div class="form-group form-row">
                 <div class="col-md-4">
                     <label for="prikaz"><h3>Zadajte príkaz</h3></label>
-                    <input type="text" class="form-control form-control-lg" name="prikaz" id="prikaz" placeholder="R">
+                    <input type="number" step="0.01" class="form-control form-control-lg" name="R" id="R" placeholder="R" required>
                     <small id="emailHelp" class="form-text text-muted">Sem zadajte vstupé R</small>
                 </div>
                 <div class="col-md-5 mt-5 ml-5">
@@ -74,7 +110,14 @@
                 <div class="col-1 mt-5">
                     <button type="submit" class="btn btn-outline-primary">Skompilovať</button>
                 </div>
-            </div>
+
+                </div>
+                <label for="graphDiv"><h3>Výsledok</h3></label>
+                <br>
+                <div class="col-12" id="graphDiv" style="width: 100%;height:500px">
+
+
+                </div>
         </form>
 
 
@@ -86,7 +129,77 @@
 <!-- jQuery first, then Popper.js, then Bootstrap JS -->
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
-    <script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+<script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous"></script>
+<script>$(document).ready(function() { $('body').bootstrapMaterialDesign(); });</script>
+<script>
+    var positions = <?=json_encode($positions)?>;
+    var angles = <?=json_encode($angles)?>;
+
+    var ypole = [];
+    for(var j=0;j<=200;j++){
+        ypole[j] = j;
+    }
+    var  trace1 = {
+        x: [],
+        y: [],
+        type: 'scatter',
+        name: 'Poloha tlmiču',
+        line: {
+            shape: 'spline',
+            smoothing: 1.3,
+            color: 'rgb(255,72,83)'
+        }
+    };
+
+    var  trace2 = {
+        x: [],
+        y: [],
+        type: 'scatter',
+        name: 'Poloha karoserie',
+        line: {
+            shape: 'spline',
+            smoothing: 1.3,
+            color: 'rgb(128,255,103)'
+        }
+    };
+
+
+    var data = [ trace1,trace2];
+
+    var layout = {
+        title:'Tlmič kolesa',
+        xaxis: {
+            title: 'Čas',
+            range: [0,200]
+        },
+        yaxis: {
+            title: 'R',
+            //range: [-<?=json_encode($_GET['R'])?>,<?=json_encode($_GET['R']*2)?>]
+            range: [<?=json_encode( "-0.4,0.4")?>]
+        }
+    };
+    var config = {responsive: true}
+
+    Plotly.newPlot(graphDiv, data, layout, config);
+
+    var cnt = 0;
+    var iterator = 1;
+    var interval = setInterval(function() {
+
+        var update = {
+            x: [[iterator], [iterator]],
+            y: [[positions[iterator]], [angles[iterator]]]
+        };
+
+
+        Plotly.extendTraces('graphDiv', update, [0,1]);
+        cnt++;
+        iterator++;
+
+        if(cnt === 200) clearInterval(interval);
+    }, 10);
+
+
+</script>>
 </body>
 </html>
